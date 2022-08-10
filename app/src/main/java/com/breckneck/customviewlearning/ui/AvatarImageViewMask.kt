d667622c0539e890312b57727da66017fa57921c
PavkaBrika <pavlikbrichkin@mail.ru>
2022-08-10 16:53:33 +0500

    created imageview with circle around image, started to make this with shader way

--- app/src/main/java/com/breckneck/customviewlearning/ui/AvatarImageViewMask.kt
+++ app/src/main/java/com/breckneck/customviewlearning/ui/AvatarImageViewMask.kt
@@ -1,9 +1,14 @@
 package com.breckneck.customviewlearning.ui
 
 import android.content.Context
-import android.graphics.Canvas
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
+import androidx.annotation.ColorInt
+import androidx.annotation.Px
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.toRectF
+import com.breckneck.customviewlearning.R
 import com.breckneck.customviewlearning.extention.dpToPx
 
 class AvatarImageViewMask @JvmOverloads constructor(
@@ -12,18 +17,107 @@ class AvatarImageViewMask @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : androidx.appcompat.widget.AppCompatImageView(context, attrs, defStyleAttr) {
 
-    companion object{
-        private const val DEFAULT_SIZE = 40
+    companion object {
+        private const val DEFAULT_BORDER_WIDTH = 2
+        private const val DEFAULT_BORDER_COLOR = Color.WHITE
+        private const val DEFAULT_SIZE = 140
+    }
+
+    @Px
+    var borderWidth: Float = context.dpToPx(DEFAULT_BORDER_WIDTH)
+
+    @ColorInt
+    private var borderColor: Int = Color.WHITE
+    private var initials: String = "??"
+
+    private val maskPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val borderPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val viewRect = Rect()
+    private lateinit var resultBm: Bitmap
+    private lateinit var maskBm: Bitmap
+    private lateinit var srcBm: Bitmap
+
+
+    init {
+        if (attrs != null) {
+            val ta = context.obtainStyledAttributes(attrs, R.styleable.AvatarImageViewMask)
+            borderWidth = ta.getDimension(
+                R.styleable.AvatarImageViewMask_aiv_borderWidth,
+                context.dpToPx(DEFAULT_BORDER_WIDTH)
+            )
+
+            borderColor = ta.getColor(R.styleable.AvatarImageViewMask_aiv_borderColor, DEFAULT_BORDER_COLOR)
+            initials = ta.getString(R.styleable.AvatarImageViewMask_aiv_initials) ?: "??"
+        }
+
+        scaleType = ScaleType.CENTER_CROP
+        setup()
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        Log.e("TAG", "onMeasure width: ${MeasureSpec.toString(widthMeasureSpec)}, " +
-                "height: ${MeasureSpec.toString(heightMeasureSpec)}")
+        Log.e(
+            "TAG", "onMeasure width: ${MeasureSpec.toString(widthMeasureSpec)}, " +
+                    "height: ${MeasureSpec.toString(heightMeasureSpec)}"
+        )
 
         val initSize = resolveDefaultSize(spec = widthMeasureSpec)
         setMeasuredDimension(initSize, initSize)
         Log.e("TAG", "onMeasure after set size: $measuredWidth $measuredHeight")
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        Log.e("TAG", "onSizeChanged")
+        if (w == 0) return
+        with(viewRect) {
+            left = 0
+            top = 0
+            right = w
+            bottom = h
+        }
+
+        prepareBitmaps(w = w, h = h)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+//        super.onDraw(canvas)
+        Log.e("TAG", "onDraw")
+        canvas!!.drawBitmap(resultBm, viewRect, viewRect, null)
+
+        val half = (borderWidth / 2).toInt()
+        viewRect.inset(half, half)
+        canvas.drawOval(viewRect.toRectF(), borderPaint)
+    }
+
+    private fun setup() {
+        with(maskPaint) {
+            color = Color.RED
+            style = Paint.Style.FILL
+        }
+
+        with(borderPaint) {
+            style = Paint.Style.STROKE
+            strokeWidth = borderWidth
+            color = borderColor
+        }
+    }
+
+    private fun prepareBitmaps(w: Int, h: Int) {
+        //prepare buffer this
+        maskBm = Bitmap.createBitmap(w, h, Bitmap.Config.ALPHA_8)
+        resultBm = maskBm.copy(Bitmap.Config.ARGB_8888, true)
+        val maskCanvas = Canvas(maskBm)
+        maskCanvas.drawOval(viewRect.toRectF(), maskPaint)
+        maskPaint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        srcBm = drawable.toBitmap(w, h, Bitmap.Config.ARGB_8888)
+
+        val resultCanvas = Canvas(resultBm)
+
+        resultCanvas.drawBitmap(maskBm, viewRect, viewRect, null)
+        resultCanvas.drawBitmap(srcBm, viewRect, viewRect, maskPaint)
+
+
     }
 
     private fun resolveDefaultSize(spec: Int): Int {
@@ -33,16 +127,6 @@ class AvatarImageViewMask @JvmOverloads constructor(
             MeasureSpec.EXACTLY -> MeasureSpec.getSize(spec)
             else -> MeasureSpec.getSize(spec)
         }
-    }
-
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        Log.e("TAG", "onSizeChanged")
-    }
-
-    override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
-        Log.e("TAG", "onDraw")
     }
 
 }
