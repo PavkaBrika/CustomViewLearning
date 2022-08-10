0d87cf79f7259ecad4116f38992f27513ba40a19
PavkaBrika <pavlikbrichkin@mail.ru>
2022-08-10 18:32:27 +0500

    created view in shader way (simpler and faster)

---
+++ app/src/main/java/com/breckneck/customviewlearning/ui/AvatarImageViewShader.kt
@@ -0,0 +1,113 @@
+package com.breckneck.customviewlearning.ui
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.util.Log
+import androidx.annotation.ColorInt
+import androidx.annotation.Px
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.toRectF
+import com.breckneck.customviewlearning.R
+import com.breckneck.customviewlearning.extention.dpToPx
+
+class AvatarImageViewShader @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : androidx.appcompat.widget.AppCompatImageView(context, attrs, defStyleAttr) {
+
+    companion object {
+        private const val DEFAULT_BORDER_WIDTH = 2
+        private const val DEFAULT_BORDER_COLOR = Color.WHITE
+        private const val DEFAULT_SIZE = 140
+    }
+
+    @Px
+    var borderWidth: Float = context.dpToPx(DEFAULT_BORDER_WIDTH)
+
+    @ColorInt
+    private var borderColor: Int = Color.WHITE
+    private var initials: String = "??"
+
+    private val avatarPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val borderPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val viewRect = Rect()
+
+
+    init {
+        if (attrs != null) {
+            val ta = context.obtainStyledAttributes(attrs, R.styleable.AvatarImageViewShader)
+            borderWidth = ta.getDimension(
+                R.styleable.AvatarImageViewShader_aivs_borderWidth,
+                context.dpToPx(DEFAULT_BORDER_WIDTH)
+            )
+
+            borderColor = ta.getColor(R.styleable.AvatarImageViewShader_aivs_borderColor, DEFAULT_BORDER_COLOR)
+            initials = ta.getString(R.styleable.AvatarImageViewShader_aivs_initials) ?: "??"
+        }
+
+        scaleType = ScaleType.CENTER_CROP
+        setup()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        Log.e(
+            "TAG", "onMeasure width: ${MeasureSpec.toString(widthMeasureSpec)}, " +
+                    "height: ${MeasureSpec.toString(heightMeasureSpec)}"
+        )
+
+        val initSize = resolveDefaultSize(spec = widthMeasureSpec)
+        setMeasuredDimension(initSize, initSize)
+        Log.e("TAG", "onMeasure after set size: $measuredWidth $measuredHeight")
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        Log.e("TAG", "onSizeChanged")
+        if (w == 0) return
+        with(viewRect) {
+            left = 0
+            top = 0
+            right = w
+            bottom = h
+        }
+
+        prepareShader(w = w, h = h)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+//        super.onDraw(canvas)
+        Log.e("TAG", "onDraw")
+        canvas!!.drawOval(viewRect.toRectF(), avatarPaint)
+
+        val half = (borderWidth / 2).toInt()
+        viewRect.inset(half, half)
+        canvas.drawOval(viewRect.toRectF(), borderPaint)
+    }
+
+    private fun setup() {
+        with(borderPaint) {
+            style = Paint.Style.STROKE
+            strokeWidth = borderWidth
+            color = borderColor
+        }
+    }
+
+    private fun prepareShader(w: Int, h: Int) {
+        //prepare shader this
+        val srcBm = drawable.toBitmap(w, h, Bitmap.Config.ARGB_8888)
+        avatarPaint.shader = BitmapShader(srcBm, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+
+    }
+
+    private fun resolveDefaultSize(spec: Int): Int {
+        return when (MeasureSpec.getMode(spec)) {
+            MeasureSpec.UNSPECIFIED -> context.dpToPx(DEFAULT_SIZE).toInt() //resolveDefaultSize()
+            MeasureSpec.AT_MOST -> MeasureSpec.getSize(spec)
+            MeasureSpec.EXACTLY -> MeasureSpec.getSize(spec)
+            else -> MeasureSpec.getSize(spec)
+        }
+    }
+}
